0a338d77e3f97f2b3f76b24b4b4f37d6a3affbf7
akay <aungkhantko@protonmail.com>
2020-07-25 18:53:16 -0700

    Installed GDT

--- init/gdt.c
+++ init/gdt.c
@@ -31,3 +31,32 @@ struct gdt_entry gdt[3];
 struct gdt_ptr gp;
 
 extern void gdt_flush();
+
+void gdt_set_descriptor(int num, unsigned long base, unsigned long limit, 
+		unsigned char access, unsigned char gran) {
+	gdt[num].base_low = (base & 0xFFFF);
+	gdt[num].base_middle = (base >> 16) & 0xFF;
+	gdt[num].base_high = (base >> 24) & 0xFF;
+
+	gdt[num].limit_low = (limit & 0xFFFF);
+	gdt[num].granularity = (limit >> 16) & 0xF;
+
+	gdt[num].granularity |= gran & 0xF0;
+	gdt[num].access = access;
+}
+
+void gdt_install() {
+	gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
+	gp.base = &gdt;
+
+	/* Null descriptor */
+	gdt_set_descriptor(0, 0, 0, 0, 0);
+
+	/* Code segment */
+	gdt_set_descriptor(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+
+	/* Data segment */
+	gdt_set_descriptor(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+
+	gdt_flush();
+}
